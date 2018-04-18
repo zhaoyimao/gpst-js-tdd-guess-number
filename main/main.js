3b1b0982dcd40d88502708ee3b4fd77717ab606c
zhaoyimao <476375361@qq.com>
2018-04-18 09:03:24 +0800

    finished

--- main/main.js
+++ main/main.js
@@ -1,6 +1,36 @@
 // Write your cade below:
-function main() {};
+module.exports=function main(system_input,user_input){
+    let number_a=getA(system_input,user_input);
+    //console.log("a="+number_a);
+    let number_b=getB(system_input,user_input);
+    //console.log(number_b);
+   return printOut(number_a,number_b);
+}
+function getA(system_input,user_input){
+    let a_count=0;
+    for(let i in system_input){
+        user_input.filter((value,index)=>index==i && system_input[i]==value && a_count++);
+    }
+    return a_count;
+}
+function getB(system_input,user_input){
+    let b_count=0;
+    for(let i in system_input){
+        user_input.filter((value,index)=>{
+            if(value==system_input[i] && i!=index){
+//console.log("值为"+system_input[i]+"i="+i);
+                delete system_input[i];
+                return b_count++;
+            }
+        });
+    }
+   // console.log("b"+b_count);
+    return b_count;
+}
 
-module.exports = {
-    main
+function printOut(number_a,number_b){
+    let str='';
+    str=`${number_a}A${number_b}B`;
+   // console.log("str="+str);
+    return str;
 }
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,23 @@
 const main = require('../main/main');
 
 describe('main()', () => {
+ it('系统为“1234”,用户输入”1234"',()=>{
+     let system_input=[1,2,3,4];
+     let user_input=[1,2,3,4];
+     let result=main(system_input,user_input);
+     expect(result).toEqual('4A0B');
+ });
+ it('系统给出“1234",用户输入”4321"',()=>{
+     let system_input=[1,2,3,4];
+     let user_input=[4,3,2,1];
+     let result=main(system_input,user_input);
+     expect(result).toEqual('0A4B');
+ });
+ it('系统给出“2431",用户输入"3321"',()=>{
+     let system_input=[2,4,3,1];
+     let user_input=[3,3,2,1];
+     let result=main(system_input,user_input);
+     expect(result).toEqual('1A2B');
 
+ });
 });
